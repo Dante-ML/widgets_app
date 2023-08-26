3f4d4b159ac2e3964b763fd6aec3b40899cda9ce
Dante-ML <emilianopr.ofi@gmail.com>
2023-08-25 18:36:47 -0600

    modificamos estilos de appBar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.pinkAccent,
+  Colors.orangeAccent,
+  Colors.tealAccent,
+  Colors.yellowAccent
+];
+
+class AppTheme {
+  final int selectedColor;  
+
+  AppTheme({
+    this.selectedColor = 0
+    }): assert(selectedColor >=0,'selectedColor must be > 0'),
+        assert(selectedColor < colorList.length,'selectedColor must be <= ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme:const AppBarTheme(
+      centerTitle: false,
+    )
+  );
+}
