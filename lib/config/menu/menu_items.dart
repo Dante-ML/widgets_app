1900f51e9cc2d36e527b401c31436d08911ceb50
Dante-ML <emilianopr.ofi@gmail.com>
2023-08-31 16:06:38 -0600

    animated container

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -45,4 +45,11 @@ const List<MenuItem> appMenuItems = [
     url: '/snackbars', 
     icon: Icons.info_outline,
   ),
+
+  MenuItem(
+    tittle: 'Animated container', 
+    subTittle: 'Stateful widget animado', 
+    url: '/animated', 
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
 ];
