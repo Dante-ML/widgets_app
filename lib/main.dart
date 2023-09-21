df3703b7e8bcb2496a34fbecba7880137513547b
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2023-09-21 16:50:17 -0600

    stateNotifier empezado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import '../../presentation/screens/screens.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
-import 'package:widgets_app/config/theme/app_theme.dart';
+//import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() {
   runApp(
@@ -18,13 +18,19 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    bool isDarkMode = ref.watch(isDarkModeProvider);
-    int selectedColor = ref.watch(selectedIndexColorProvider);
+    //bool isDarkMode = ref.watch(isDarkModeProvider);
+    //int selectedColor = ref.watch(selectedIndexColorProvider);
+
+    //appTheme nos permite utilizar los valores de nuestra clase app_theme
+    final appTheme = ref.watch(themeNotifierProvider);
     return MaterialApp.router(
       title: 'Flutter Widgets',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: selectedColor, isDarkMode: isDarkMode).getTheme(),
+      //Ojo el que gobierna el tema ahora es el themeNotifierProvider
+      theme: appTheme.getTheme()
+      //Como seria antes:
+      //AppTheme(selectedColor: appTheme.selectedColor, isDarkMode: appTheme.isDarkMode).getTheme(),
 
     );
   }
--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -2,13 +2,35 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-final isDarkModeProvider = StateProvider((ref) => false);
-
-//Listado de colores inmutables
+//Listado de colores INMUTABLES
 final colorListProvider = Provider((ref) => colorList);
+
+//Bool  Pieza de estado
+final isDarkModeProvider = StateProvider((ref) => false);
 
 //Entero
 final selectedIndexColorProvider = StateProvider<int>((ref) => 0);
+
+//objeto de tipo Apptheme personalizado
+//mantener estado de un objeto mas elaborado
+//El que controla es el ThemeNotifier y el estado es una instancia de AppTheme
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier,AppTheme>(
+  (ref) => ThemeNotifier(),
+  );
+
+class ThemeNotifier extends StateNotifier<AppTheme>{
+  //Creación de la primera instancia de AppTheme, tenemos valores por defecto
+  //STATE = Estadp = new AppTheme();
+  ThemeNotifier():super(AppTheme());
+
+  void toggleDarkmode(){
+    
+  }
+
+  void changeColorIndex(int colorIndex){
+
+  }
+}
 
 /*
 final isDarkModeProvider = NotifierProvider<Toggle, bool>(Toggle.new);
